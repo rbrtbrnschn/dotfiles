7765c476c87e8c829584f65890781702fa94b02e
Robert Bornschein <robertbornschein@mailbox.tu-dresden.de>
2021-08-15 17:56:40 +0200

    changed loading of aliases

--- .config/bash.d/exports.sh
+++ .config/bash.d/exports.sh
@@ -2,5 +2,6 @@
 
 # Exports #
 export EDITOR=nvim
+export ALIASES_PATH="/tmp/aliases.sh:$HOME/.config/bash.d/aliases.sh"
 PS1='[\u@\h \W]\$ '
 # Exports #
--- .config/bash.d/misc.sh
+++ .config/bash.d/misc.sh
@@ -3,3 +3,8 @@
 # Miscellaneous #
 set -o vi
 # Miscellaneous #
+
+# Load Aliases #
+for path in ${ALIASES_PATH//:/ }; do
+  test -f $path && . $path
+done
